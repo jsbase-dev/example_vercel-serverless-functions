a565cf1e6bb7b8c8653b2ba20ed4e35b0f78f43b
Andre JS <andre.siedersleben@gmail.com>
2020-07-06 05:21:36 +0200

    Update write.js

--- api/write.js
+++ api/write.js
@@ -1,11 +1,13 @@
 const fs = require('fs-extra');
+const path = require('path')
 const CBOR = require('cbor-sync');
 
-const data = require(`${__dirname}/../store/data.json`);
+const data = require(path.join(__dirname, '/../store/data.json'));
 const buffer = CBOR.encode(data);
 
-fs.writeFile(
-	`${__dirname}/../store/store.cbor`,
+module.exports = function (req, res) {
+    fs.writeFile(
+        path.join(__dirname, '/../store/store.cbor')
 	buffer,
 	function (err) {
 		if (err) {
@@ -14,4 +16,7 @@ fs.writeFile(
 
 		console.log(`  ➤  📰 write data (in file): ${JSON.stringify(buffer)}`);
 	}
-);
+    );
+
+    return res.json(JSON.stringyfy(CBOR.encode(data)));
+};
