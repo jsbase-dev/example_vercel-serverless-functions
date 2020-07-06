74d6f9cfd8b06444e15e4a61d2263b436b2222a5
Andre JS <andre.siedersleben@gmail.com>
2020-07-06 05:28:07 +0200

    Update read.js

--- api/read.js
+++ api/read.js
@@ -1,13 +1,15 @@
 const fs = require('fs-extra');
+const os = require('os');
 const CBOR = require('cbor-sync');
 
-fs.readFile(
-	`${__dirname}/../store/store.cbor`,
+module.exports = function (req, res) {
+    fs.readFile(
+	path.join(os.tmpdir(), '/store.cbor'),
 	function (err, buffer) {
 		if (err) return console.error(err);
 
 		let data = CBOR.decode(buffer)
 
-		console.log(`  ➤  📰 read data: ${JSON.stringify(data)}`);
+		return res.json(JSON.stringify(data));
 	}
 );
