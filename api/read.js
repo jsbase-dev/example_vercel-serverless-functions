77ccca26184dc9c5b49d53480716a0bdf36c3e19
jsbase <andre.siedersleben@gmail.com>
2020-07-06 20:17:01 +0200

    Change tests

--- api/read.js
+++ api/read.js
@@ -1,19 +1,18 @@
 const fs = require('fs-extra')
 const path = require ('path')
 const os = require('os')
-const CBOR = require('cbor-sync')
 
 module.exports = function get(req, res) {
-	const data = {}
+	const json = {}
 
-	// console.log(' > tmpdir ' + os.tmpdir())
-	const storePath = path.join(__dirname, '/../store/store.cbor')
-	// console.log(' > storePath ' + storePath)
+    console.log(' > read - tmpdir ' + os.tmpdir())
+	const storePath = path.join(os.tmpdir(), '/store.json')
+	console.log(' > read - storePath ' + storePath)
 
-	fs.readFileSync(storePath, function (err, buffer) {
+	fs.readFileSync(storePath, function (err, data) {
 		if (err) { res.json(JSON.stringify({ error: err })) }
-		data = Object.assign( CBOR.decode(buffer), data )
+		json = Object.assign( data, json )
 	})
 
-    return res.json( JSON.stringify(data) )
-}
+	return res.json( JSON.stringify(json) )
+};
--- api/write.js
+++ api/write.js
@@ -1,19 +1,16 @@
 const fs = require('fs-extra')
 const path = require('path')
 const os = require('os')
-// const CBOR = require('cbor-sync')
-// fs.writeFileSync( storePath, buffer, (err) => console.log(err) )
 
 const dataPath = path.join(__dirname, '/../store/data.json')
 const data = require(dataPath)
 
 module.exports = function get(req, res) {
 	const jsonStr = JSON.stringify(data)
-	// console.log(' > json:', jsonStr)
 
-	// console.log(' > tmpdir ' + os.tmpdir())
+	console.log(' > write - tmpdir ' + os.tmpdir())
 	const storePath = path.join(os.tmpdir(), '/store.json')
-	// console.log(' > storePath ' + storePath)
+	console.log(' >  write - storePath ' + storePath)
 
 	fs.writeFileSync(storePath, jsonStr, function (err) { return res.json(JSON.stringify({ error: err })) })
 
