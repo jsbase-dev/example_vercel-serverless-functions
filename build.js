eb9995473d0dbc8bbc5aa5bf3618323f0b17919a
jsbase <andre.siedersleben@gmail.com>
2020-07-07 02:40:55 +0200

    Change tryout for polka.js

--- build.js
+++ build.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 const serverJs = path.join(`
 const polka = require("polka")
-polka().get("/", (req, res) => { res.end("<h1>✌ Hello party people!</h1>") })
+polka().get("/", (req, res) => { res.end("<h1>Hello party people!</h1>") })
 .listen(3000, err => { if (err) throw err; console.log("Running on localhost:3000") });
 `)
 
