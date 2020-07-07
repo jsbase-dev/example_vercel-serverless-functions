9385eb80941a60ce34a2d98a63e81dbc9dec75be
jsbase <andre.siedersleben@gmail.com>
2020-07-07 02:33:59 +0200

    Change tryout for polka.js

--- index.js
+++
@@ -1,4 +0,0 @@
-
-const polka = require("polka")
-polka().get("/", (req, res) => { res.end("<h1>✌ Hello party people!</h1>") })
-.listen(3000, err => { if (err) throw err; console.log("Running on localhost:3000") });
